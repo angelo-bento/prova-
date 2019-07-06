c0b575848b18a0698bca4bc2f53ec42e7cea0614
angelo-bento <51722762+angelo-bento@users.noreply.github.com>
2019-07-05 23:35:32 -0300

    Add files via upload

---
+++ angeloB_aval_02_07.py
@@ -0,0 +1,9 @@
+# meses = "janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "novembro", "novembro", "dezembro" "]
+
+mes =  str ( input ( " insira o nascimento do nascimento de Angelo Bento: " ))
+dia =  int ( input ( " insira o Dia do Seu aniversario: " ))
+
+if mes == "18/09/2000":
+    print ( " Estação do aniversário de angelo é inverno! " )
+else :
+    print ( " angelo não nasceu nessa, opção invalida " )
