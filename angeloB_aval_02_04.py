08fdb44f674fa198ff502276a1d15ad113253a94
angelo-bento <51722762+angelo-bento@users.noreply.github.com>
2019-07-05 23:25:39 -0300

    Add files via upload

---
+++ angeloB_aval_02_04.py
@@ -0,0 +1,24 @@
+lista = [ 1 , 8 , 0 , 9 , 2 , 0 , 0 , 0 ]
+print (lista)
+del (lista [ 2 ])
+print (lista)
+
+del (lista [ 2 ])
+print (lista)
+
+del (lista [ 2 ])
+print (lista)
+
+del (lista [ 2 ])
+print (lista)
+
+del (lista [ 2 ])
+print (lista)
+
+del (lista [ 2 ])
+print (lista)
+
+del (lista [ 1 ])
+print (lista)
+del (lista [ 0 ])
+print (lista)
