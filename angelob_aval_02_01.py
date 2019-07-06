5b5d4ab4318614a34eb1f24c2e7352d5f0ffbdc1
angelo-bento <51722762+angelo-bento@users.noreply.github.com>
2019-07-05 23:17:07 -0300

    Add files via upload

---
+++ angelob_aval_02_01.py
@@ -0,0 +1,9 @@
+dia1 =  int (entrada ("Dia 1:"))
+mes1 =  int ( entrada ("Mês 1:"))
+ano1 =  int ( input ( " Ano 1: " ))
+
+dia2 =  int ( entrada ( " Dia 2: " ))
+mes2 =  int ( entrada ( " Mês 2: " ))
+ano2 =  int ( input ( " Ano 2: " ))
+
+print ((ano2 - ano1) *  365  + ((mes2 - mes1) *  30 ) + (dia2 - dia1))
